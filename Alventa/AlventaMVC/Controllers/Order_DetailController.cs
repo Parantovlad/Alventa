4c40352a49c3992cf294c4a449b69a23163c89d2
parantovlad <parantovlad@mail.ru>
2017-12-07 09:21:21 +0700

    Add send email

--- Alventa/AlventaMVC/Controllers/Order_DetailController.cs
+++ Alventa/AlventaMVC/Controllers/Order_DetailController.cs
@@ -12,6 +12,7 @@ using AlventaDB.Models;
 using AlventaDB.Repos;
 using PagedList.Mvc;
 using PagedList;
+using System.Net.Mail;
 
 namespace AlventaMVC.Controllers
 {
@@ -19,33 +20,49 @@ namespace AlventaMVC.Controllers
     {
         private readonly Order_DetailRepo repo = new Order_DetailRepo();
 
-        public async Task<ActionResult> Index(int? page)
+        public async Task<ActionResult> Index(int? page, string from, string to)
         {
             int pageSize = 20;
             int pageNumber = (page ?? 1);
+            IEnumerable<Order_Detail> order_Detail;
 
-            var order_Detail = await repo.GetAllAsync();
-
-            return View(order_Detail.ToPagedList(pageNumber,pageSize));
-        }
-
-        public async Task<ActionResult> Details(string startDate, string finishDate)
-        {
-            if (startDate == null || finishDate == null)
+            if (from == null || to == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                order_Detail = await repo.GetAllAsync();
+            }
+            else
+            {
+                order_Detail = from od in await repo.GetAllAsync()
+                               where od.Order.OrderDate >= Convert.ToDateTime(@from) &&
+                                     od.Order.OrderDate <= Convert.ToDateTime(to)
+                               select od;
             }
 
-            var order_Detail = from od in await repo.GetAllAsync()
-                               where od.Order.OrderDate >= Convert.ToDateTime(startDate) && 
-                                     od.Order.OrderDate <= Convert.ToDateTime(finishDate)
-                               select od;
             if (order_Detail == null)
             {
                 return HttpNotFound();
             }
 
-            return View("Index", order_Detail);
+            return View(order_Detail.ToPagedList(pageNumber,pageSize));
+        }
+
+        [HttpPost]
+        public ActionResult Send(string email)
+        {
+            MailAddress from = new MailAddress("parantovlad@mail.ru");
+            MailAddress to = new MailAddress(email);
+
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Test";
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            m.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 587);
+            smtp.Credentials = new NetworkCredential("parantovlad@mail.ru", "par110587");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+
+            return PartialView();
         }
 
         protected override void Dispose(bool disposing)
--- Alventa/AlventaMVC/Scripts/myJS.js
+++ Alventa/AlventaMVC/Scripts/myJS.js
@@ -5,8 +5,7 @@
             defaultDate: "+1w",
             changeMonth: true,
             changeYear: true,
-            numberOfMonths: 3,
-            regional: "ru"
+            numberOfMonths: 3
         })
         .on("change", function () {
             to.datepicker("option", "minDate", getDate(this));
@@ -15,12 +14,30 @@
           defaultDate: "+1w",
           changeMonth: true,
           changeYear: true,
-          numberOfMonths: 3,
-          regional: "ru"
+          numberOfMonths: 3
       })
       .on("change", function () {
           from.datepicker("option", "maxDate", getDate(this));
       });
+
+    $.datepicker.regional['ru'] = {
+        prevText: 'Пред',
+        nextText: 'След',
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+        'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+        'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
 
     function getDate(element) {
         var date;
