6de08238d4340c810b6b16c30bcc541227372228
parantovlad <parantovlad@mail.ru>
2017-12-08 09:19:45 +0700

    Не много не успеваю

--- Alventa/AlventaMVC/Scripts/myJS.js
+++ Alventa/AlventaMVC/Scripts/myJS.js
@@ -20,24 +20,24 @@
           from.datepicker("option", "maxDate", getDate(this));
       });
 
-    $.datepicker.regional['ru'] = {
-        prevText: 'Пред',
-        nextText: 'След',
-        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-        'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
-        'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        weekHeader: 'Не',
-        dateFormat: 'dd/mm/yy',
+    $.datepicker.regional["ru"] = {
+        prevText: "Пред",
+        nextText: "След",
+        monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+        monthNamesShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+        "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+        dayNames: ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"],
+        dayNamesShort: ["вск", "пнд", "втр", "срд", "чтв", "птн", "сбт"],
+        dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        weekHeader: "Не",
+        dateFormat: "dd/mm/yy",
         firstDay: 1,
         isRTL: false,
         showMonthAfterYear: false,
-        yearSuffix: ''
+        yearSuffix: ""
     };
-    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $.datepicker.setDefaults($.datepicker.regional["ru"]);
 
     function getDate(element) {
         var date;
@@ -48,5 +48,20 @@
         }
 
         return date;
-    }
+    };
+
+    $("#email").blur(function () {
+        if ($(this).val() != "") {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if (pattern.test($(this).val())) {
+                $(this).css({ "border": "1px solid #569b44" });
+                $("send").removeAttr("disabled");
+            } else {
+                $(this).css({ "border": "1px solid #ff0000" });
+            }
+        } else {
+            $(this).css({ "border": "1px solid #ff0000" });
+        }
+    });
+    
 });
