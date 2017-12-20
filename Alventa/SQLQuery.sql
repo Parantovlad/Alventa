2778dcb389bb5db095e35c0de90b79ba2e0a4557
parantovlad <parantovlad@mail.ru>
2017-12-20 07:19:31 +0700

    Add SP in SQL

--- Alventa/SQLQuery.sql
+++ Alventa/SQLQuery.sql
@@ -1,11 +1,15 @@
-﻿SELECT Q1.FIO, Q1.[Quantity Orders], Q2.[Sold Items], Q1.[Quantity Customers], Q2.[Order Sum], Q2.[Average Cost Of Product], Q2.[Order Average], Q2.[Average Sum On Customer] FROM
+﻿CREATE PROCEDURE spGetSalesReport
+	@From date,
+	@To date
+AS
+SELECT Q1.FIO, Q1.[Quantity Orders], Q2.[Sold Items], Q1.[Quantity Customers], Q2.[Order Sum], Q2.[Average Cost Of Product], Q2.[Order Average], Q2.[Average Sum On Customer] FROM
 	(SELECT 
 	LastName + FirstName as FIO, 
 	COUNT(o.OrderID) as [Quantity Orders], 
 	COUNT(DISTINCT o.CustomerID) as [Quantity Customers]
 	FROM Employees as e
 	INNER JOIN Orders as o ON o.EmployeeID = e.EmployeeID
-	--WHERE o.OrderDate >= '19960704' AND o.OrderDate <= '19960705'
+	WHERE o.OrderDate >= @From AND o.OrderDate <= @To
 	GROUP BY LastName + FirstName) Q1,
 	(SELECT 
 	LastName + FirstName as FIO, 
@@ -17,7 +21,10 @@
 	FROM Employees as e
 	INNER JOIN Orders as o ON o.EmployeeID = e.EmployeeID
 	INNER JOIN [Order Details] as od ON od.OrderID = o.OrderID
-	--WHERE o.OrderDate >= '19960704' AND o.OrderDate <= '19960705'
+	WHERE o.OrderDate >= @From AND o.OrderDate <= @To
 	GROUP BY LastName + FirstName) Q2
 WHERE Q1.FIO = Q2.FIO;
+GO
 
+EXECUTE spGetSalesReport '19960704', '19970805'
+GO
